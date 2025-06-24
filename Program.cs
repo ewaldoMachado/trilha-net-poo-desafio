f16214f99d9dbe0a9849441d97c4a8ae3e058987
Ewaldo Machado <ewaldo2016@gmail.com>
2025-06-23 21:07:21 -0300

    Resolução do desafio de código trilha-net-poo-deafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "modelo 1100", imei: "012345678-9", memoria: 32);
+nokia.Ligar();
+nokia.Apresentar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "54321", modelo: "modelo 14", imei: "098765432-1", memoria: 128);
+iphone.ReceberLigacao();
+iphone.Apresentar();
+iphone.InstalarAplicativo("Youtube");
